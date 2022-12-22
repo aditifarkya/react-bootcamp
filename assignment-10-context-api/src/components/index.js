6c327709f796a17b66210b2390c07d119e748ee9
Aditi Farkya <118219855+aditifarkya@users.noreply.github.com>
2022-12-22 14:48:19 +0530

    feat(assignment): deploy app to github pages (#13)

---
+++ assignment-10-context-api/src/components/index.js
@@ -0,0 +1,13 @@
+import CardComponent from "./card-component";
+import HeaderComponent from "./header-component";
+import SearchComponent from "./search-component";
+import BodyComponent from "./body-component";
+import ErrorComponent from "./error-component";
+
+export {
+  CardComponent,
+  HeaderComponent,
+  SearchComponent,
+  BodyComponent,
+  ErrorComponent,
+};
